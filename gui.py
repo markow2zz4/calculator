cc5d9365d9af245e55dcb9f8d911837c8aaff59c
markow_xxl <albertnov@internet.ru>
2023-08-27 02:16:10 +0700

    added work of all buttons

--- gui.py
+++ gui.py
@@ -82,7 +82,7 @@ for i in range(6):
     by += 70
 
 def clearLast():
-    """ for CE func (not completed) """
+    """ for CE func (completed) """
     chis = entry_field.get()
     number = 0
     counter = len(chis)
@@ -103,6 +103,10 @@ def eq():
             raise Exception("Error")
         else:
             entry_field.delete(0, END)
+
+            if("," in chis):
+                chis = chis.replace(",", ".")
+                
             example = eval(chis)
 
             if example == float(f'{int(example)}.0'):
@@ -114,7 +118,7 @@ def eq():
                 
     except Exception as e:
         
-        if(chis == "Error"):
+        if(chis == "Error" or chis == ""):
             entry_field.delete(0, END)
         
         else:
@@ -162,6 +166,120 @@ def rad():
             example = float(f'{example:.6}')
         entry_field.delete(0, END)
         entry_field.insert(0, example)
+
+def multiple():
+    chis = entry_field.get()
+
+    if(chis[len(chis) -1] in "*+-%/"):
+        #solve a much multiples
+        entry_field.delete(len(entry_field.get())-1)
+        entry_field.insert(END, "*")
+    else:
+        entry_field.insert(END, "*")
+
+def plus():
+    chis = entry_field.get()
+
+
+    if(chis[len(chis) -1] in "*+-%/"):
+        #solve a much pluses
+        entry_field.delete(len(entry_field.get())-1)
+        entry_field.insert(END, "+")
+    else:
+        entry_field.insert(END, "+")
+
+def comma():
+    chis = entry_field.get()
+    if(chis[len(chis) -1] not in "*+-%/,"):
+        entry_field.insert(END, ",")
+    else:
+        entry_field.insert(END, "")
+
+def change_sign2():
+    chis = entry_field.get()
+    number = ""
+
+    counter = len(chis)
+    temp = 0
+        
+    if("(" in chis and ")" in chis): chis = chis.replace("(", "").replace(")","")
+    try:
+        while chis[counter - 1].isdigit() == True:
+            print("shicso - ", chis[counter - 1])
+            number += chis[counter - 1]
+            counter -= 1
+            temp += 1
+    except:
+        #kostyl
+        pass
+        # entry_field.delete(0, END)
+        # entry_field.insert(entry_field.get().replace("-", "+"))
+
+    if chis.isdigit():
+        number = int(chis)
+        entry_field.delete(0, END)
+        entry_field.insert(0, number)
+    else:
+        for i in range(temp+1+2):
+            entry_field.delete(len(entry_field.get()) - 1)
+
+
+        
+        chis = chis.replace("-", "", 1).replace("-", "+")
+        
+        entry_field.delete(0, END)
+        entry_field.insert(0, chis)
+        
+
+def change_sign():
+    chis = entry_field.get()
+    counter = len(chis)
+    number = 0
+    if(chis[len(chis) - 1] == "0"):
+        number = 1
+    else:
+        number = 0
+
+    if("(" in chis and ")" in chis):
+        chis = chis.replace("(", "").replace(")","")
+        print(chis)
+
+    try:
+        #not completed
+        if(str(int(chis) * -1).isdigit() == True or chis.isdigit() == True):
+            number = int(chis)
+
+            entry_field.delete(0, END)
+            
+
+            if(abs(int(number)) > 0):
+                entry_field.insert(END, f'{int(number) * -1}')
+            else:
+                entry_field.insert(END, f'({int(number) * -1})')
+
+        else:
+
+            if(abs(int(number)) > 0):
+                entry_field.insert(END, f'{int(number) * -1}')
+            else:
+                entry_field.insert(END, f'({int(number) * -1})')
+
+            while type(int(chis[counter-1])) == int:
+                number += int(chis[counter-1])
+                #solve with 1number
+                entry_field.delete(len(entry_field.get())-1)
+                
+                number*=10
+                print(number)
+                counter -= 1
+            number/=10
+            number = str(int(number))[::-1]
+            
+            
+        
+    
+    except:
+        pass
 #-----------------------------BTNS-----------------------------#
 dic["btn_0"].config(command=percent)
 dic["btn_1"].config(command=clearLast)
@@ -177,25 +295,23 @@ dic["btn_8"].config(command=lambda: entry_field.insert(END, "7"))
 dic["btn_9"].config(command=lambda: entry_field.insert(END, "8"))
 dic["btn_10"].config(command=lambda: entry_field.insert(END, "9"))
 
+dic["btn_11"].config(command=multiple)
+
 dic["btn_12"].config(command=lambda: entry_field.insert(END, "4"))
 dic["btn_13"].config(command=lambda: entry_field.insert(END, "5"))
 dic["btn_14"].config(command=lambda: entry_field.insert(END, "6"))
-
+dic["btn_15"].config(command=lambda: entry_field.insert(END, "-"))
 dic["btn_16"].config(command=lambda: entry_field.insert(END, "1"))
 dic["btn_17"].config(command=lambda: entry_field.insert(END, "2"))
 dic["btn_18"].config(command=lambda: entry_field.insert(END, "3"))
+
+dic["btn_19"].config(command=plus)
+dic["btn_20"].config(command=change_sign2)
 dic["btn_21"].config(command=lambda: entry_field.insert(END, "0"))
-
-
-
+dic["btn_22"].config(command=comma)
 #-----------------------------BTNS-end-----------------------------#
-
 
 equals = Button(window, width=70, height=50, borderwidth=0,background="#2E2E2E",image=btn[len(btn) - 1], command=eq)
 equals.place(x=355, y=by-70)
-
-
-
-
 
 window.mainloop()
